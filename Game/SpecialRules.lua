f3391a9fb6dd4dddcf1b2f4f7ca405bd8416ec3d
jrizzo <josh.rizzo@gmail.com>
2018-03-06 16:52:01 -0600

    some more initial changes and code review

---
+++ Game/SpecialRules.lua
@@ -0,0 +1,10 @@
+SpecialRules = {
+    -- Example special rule.
+    -- ["Mega Melta"] = function(unit) 
+    --     unit.mods[Events.shoot.shooting] = function(phase)
+    --         if phase.range < 16 then
+    --             phase.weapon.D = phase.weapon.D * 3
+    --         end
+    --     end
+    -- end
+}
